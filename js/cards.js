aa077158ad6385ad593f06635567f564b7be3434
jic <ji_camargo@yahoo.com>
2023-04-26 12:18:42 -0500

    Fix linters errors

--- js/cards.js
+++ js/cards.js
@@ -1,53 +1,53 @@
-   const arrCards = [
-    {
-      id: 'card-0',
-      cardTitle: 'Music festival',
-      cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-      img: './images/icon-concert-1.png',
-      imgHover: './images/icon-concert-2.png'
-    },
-    {
-      id: 'card-1',
-      cardTitle: 'Awards shows',
-      cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-      img: './images/icon-award-1.png',
-      imgHover: './images/icon-award-2.png'
-    },
-    {
-      id: 'card-2',
-      cardTitle: 'Musician conferences',
-      cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-      img: './images/icon-conference-1.png',
-      imgHover: './images/icon-conference-2.png'
-    },
-    {
-      id: 'card-3',
-      cardTitle: 'Networking',
-      cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-      img: './images/icon-networking-1.png',
-      imgHover: './images/icon-networking-2.png'
-    },
-    {
-      id: 'card-4',
-      cardTitle: 'Trade exposition',
-      cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-      img: './images/icon-merch-1.png',
-      imgHover: './images/icon-merch-2.png'
-    }
-  ];
-  
-  const cardcnt = document.getElementById('cards-cnt');
-  let div = null;
-  let card = null;
-  
-  for (let i = 0; i < arrCards.length; i += 1) {
-    card = arrCards[i];
-  
-    div = document.createElement('div');
-    div.classList = 'card mb-1';
-    div.id = card.id;
+const arrCards = [
+  {
+    id: 'card-0',
+    cardTitle: 'Music festival',
+    cardText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/icon-concert-1.png',
+    imgHover: './images/icon-concert-2.png',
+  },
+  {
+    id: 'card-1',
+    cardTitle: 'Awards shows',
+    cardText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/icon-award-1.png',
+    imgHover: './images/icon-award-2.png',
+  },
+  {
+    id: 'card-2',
+    cardTitle: 'Musician conferences',
+    cardText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/icon-conference-1.png',
+    imgHover: './images/icon-conference-2.png',
+  },
+  {
+    id: 'card-3',
+    cardTitle: 'Networking',
+    cardText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/icon-networking-1.png',
+    imgHover: './images/icon-networking-2.png',
+  },
+  {
+    id: 'card-4',
+    cardTitle: 'Trade exposition',
+    cardText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/icon-merch-1.png',
+    imgHover: './images/icon-merch-2.png',
+  },
+];
 
-    div.innerHTML = `<div class="card mb-1">
+const cardcnt = document.getElementById('cards-cnt');
+let div = null;
+let card = null;
+
+for (let i = 0; i < arrCards.length; i += 1) {
+  card = arrCards[i];
+
+  div = document.createElement('div');
+  div.classList = 'card mb-1';
+  div.id = card.id;
+
+  div.innerHTML = `<div class="card mb-1">
         <div class="row g-1">
           <div class="col-2 col-md-12 img-cnt p-0">
             <img src="${card.img}" class="img-fluid img-card ms-2 ms-md-0" alt="${card.cardTitle} icon.">
@@ -59,60 +59,58 @@
             <p class="card-text">${card.cardText}</p>
           </div>
         </div>
-      </div>` 
-    cardcnt.appendChild(div);  
-  }
-  
+      </div>`;
+  cardcnt.appendChild(div);
+}
 
-/**************************** SPEAKERS **********************************/
+/** ************************** SPEAKERS ********************************* */
 
 const arrSpeakers = [
   {
     id: 'speaker-0',
     name: 'Adam Shomer',
     title: 'Vice President, Business Development, Create Music Group',
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/adam-shomer-250x250.jpg'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/adam-shomer-250x250.jpg',
   },
   {
     id: 'speaker-1',
-    name:"Adam Rogers",
-    title: "Head of Studio and VP of Creative , Departure Lounge",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/adam-rogers-250x250.jpg'
+    name: 'Adam Rogers',
+    title: 'Head of Studio and VP of Creative , Departure Lounge',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/adam-rogers-250x250.jpg',
   },
   {
     id: 'speaker-2',
-    name:"Adesegun Adeosun",
-    title: "Founder & CEO and Co-Founder of Afro Nation, Smade Entertainment Group",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/adesegun-250x250.jpg'
+    name: 'Adesegun Adeosun',
+    title: 'Founder & CEO and Co-Founder of Afro Nation, Smade Entertainment Group',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/adesegun-250x250.jpg',
   },
   {
     id: 'speaker-3',
-    name:"Alex Branson",
-    title: "SVP of Music Services, Beatport",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/alex-branson-250x250.jpg'
+    name: 'Alex Branson',
+    title: 'SVP of Music Services, Beatport',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/alex-branson-250x250.jpg',
   },
   {
     id: 'speaker-4',
-    name:"Anna Ruddick",
-    title: "Music Industry & Client Services Manager, The Unison Fund",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/anna-buddick-250x250.jpg'
+    name: 'Anna Ruddick',
+    title: 'Music Industry & Client Services Manager, The Unison Fund',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/anna-buddick-250x250.jpg',
   },
   {
     id: 'speaker-5',
-    name:"Anne McKinnon",
-    title: "CEO, Ristband",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
-    img: './images/anne-mckinnon-250x250.jpg'
-  }
-]
+    name: 'Anne McKinnon',
+    title: 'CEO, Ristband',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam',
+    img: './images/anne-mckinnon-250x250.jpg',
+  },
+];
 
 const speakers = document.getElementById('speakers-cnt');
-
 
 for (let i = 0; i < arrSpeakers.length; i += 1) {
   card = arrSpeakers[i];
@@ -120,8 +118,7 @@ for (let i = 0; i < arrSpeakers.length; i += 1) {
   div = document.createElement('div');
   div.classList = 'card border-0';
   div.id = card.id;
-  div.innerHTML = 
-    `<div class="row g-4">
+  div.innerHTML = `<div class="row g-4">
       <div class="col-4 img-cnt spk-img-cnt p-0">
         <img src="${card.img}" class="img-fluid img-spk" alt="${card.name} photo.">
       </div>
@@ -131,7 +128,6 @@ for (let i = 0; i < arrSpeakers.length; i += 1) {
         <div class="spk-short-line"></div>
         <p class="spk-card-text">${card.bio}</p>
       </div>
-    </div>` 
-  speakers.appendChild(div);  
+    </div>`;
+  speakers.appendChild(div);
 }
-
