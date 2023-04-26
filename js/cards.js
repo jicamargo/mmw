bfdf693500b07376a71b8d2210efd9f8eb7fb2ab
jic <ji_camargo@yahoo.com>
2023-04-25 22:17:25 -0500

    add styles and rwd to page 1 and 2

--- js/cards.js
+++ js/cards.js
@@ -15,7 +15,7 @@
     },
     {
       id: 'card-2',
-      cardTitle: 'Conferences',
+      cardTitle: 'Musician conferences',
       cardText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
       img: './images/icon-conference-1.png',
       imgHover: './images/icon-conference-2.png'
@@ -36,7 +36,7 @@
     }
   ];
   
-  const program = document.getElementById('cards-cnt');
+  const cardcnt = document.getElementById('cards-cnt');
   let div = null;
   let card = null;
   
@@ -44,23 +44,94 @@
     card = arrCards[i];
   
     div = document.createElement('div');
-      div.innerHTML = `
-        <div class="card mb-1" >
-          <div class="row g-1">
-            <div class="col-2 col-md-12 img-cnt p-0">
-              <img src="${card.img}" class="img-fluid " alt="${card.cardTitle} icon.">
-            </div>
-            <div class="card-body col-3 col-md-12">
-              <h5 class="card-title">${card.cardTitle}</h5>
-            </div>
-            <div class="col-7 col-md-12">
-              <p class="card-text">${card.cardText}</p>
-            </div>
-          </div>
-        </div>` 
+    div.classList = 'card mb-1';
+    div.id = card.id;
 
-      console.log("entre ");
-    program.appendChild(div);
-    
+    div.innerHTML = `<div class="card mb-1">
+        <div class="row g-1">
+          <div class="col-2 col-md-12 img-cnt p-0">
+            <img src="${card.img}" class="img-fluid ms-2 ms-md-0" alt="${card.cardTitle} icon.">
+          </div>
+          <div class="card-body col-4 col-md-12 text-center align-self-center">
+            <h5 class="card-title">${card.cardTitle}</h5>
+          </div>
+          <div class="col-6 col-md-12 py-4 px-2 text-center">
+            <p class="card-text">${card.cardText}</p>
+          </div>
+        </div>
+      </div>` 
+    cardcnt.appendChild(div);  
   }
   
+
+/**************************** SPEAKERS **********************************/
+
+const arrSpeakers = [
+  {
+    id: 'speaker-0',
+    name: 'Adam Shomer',
+    title: 'Vice President, Business Development, Create Music Group',
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/adam-shomer-250x250.jpg'
+  },
+  {
+    id: 'speaker-1',
+    name:"Adam Rogers",
+    title: "Head of Studio and VP of Creative , Departure Lounge",
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/adam-rogers-250x250.jpg'
+  },
+  {
+    id: 'speaker-2',
+    name:"Adesegun Adeosun",
+    title: "Founder & CEO and Co-Founder of Afro Nation, Smade Entertainment Group",
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/adesegun-250x250.jpg'
+  },
+  {
+    id: 'speaker-3',
+    name:"Alex Branson",
+    title: "SVP of Music Services, Beatport",
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/alex-branson-250x250.jpg'
+  },
+  {
+    id: 'speaker-4',
+    name:"Anna Ruddick",
+    title: "Music Industry & Client Services Manager, The Unison Fund",
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/anna-buddick-250x250.jpg'
+  },
+  {
+    id: 'speaker-5',
+    name:"Anne McKinnon",
+    title: "CEO, Ristband",
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam",
+    img: './images/anne-mckinnon-250x250.jpg'
+  }
+]
+
+const speakers = document.getElementById('speakers-cnt');
+
+
+for (let i = 0; i < arrSpeakers.length; i += 1) {
+  card = arrSpeakers[i];
+
+  div = document.createElement('div');
+  div.classList = 'card border-0';
+  div.id = card.id;
+  div.innerHTML = 
+    `<div class="row g-4">
+      <div class="col-4 img-cnt p-0">
+        <img src="${card.img}" class="img-fluid" alt="${card.name} photo.">
+      </div>
+      <div class="card-body col-8 text-left">
+        <h5 class="spk-card-name">${card.name}</h5>
+        <h5 class="spk-card-title">${card.title}</h5>
+        <div class="spk-short-line"></div>
+        <p class="spk-card-text">${card.bio}</p>
+      </div>
+    </div>` 
+  speakers.appendChild(div);  
+}
+
